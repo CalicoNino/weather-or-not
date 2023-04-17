e6338bc8655ce7a5f6ab675b239a1466b3fb53bd
elshenak <elshenak@gmail.com>
2023-04-17 18:58:47 -0400

    add linting

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Changed</Text>
       <StatusBar style="auto" />
     </View>
   );
