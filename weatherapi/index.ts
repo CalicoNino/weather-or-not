f9f59378b3fca13dfae405ac4a7b07cd71564dd3
elshenak <elshenak@gmail.com>
2023-04-20 07:03:16 -0400

    api class

--- weatherapi/index.ts
+++ weatherapi/index.ts
@@ -1,13 +1,53 @@
 export default class WeatherClient {
-  baseUrl: string;
+  baseUrl: URL;
 
-  latitude;
+  latitude: string;
 
-  longitude;
+  longitude: string;
 
   constructor(latitude: string, longitude: string) {
-    this.baseUrl = process.env.BASE_URl || "";
+    this.baseUrl = new URL(process.env.BASE_URl || "");
     this.latitude = latitude;
     this.longitude = longitude;
+    this.baseUrl.searchParams.append("latitude", this.latitude);
+    this.baseUrl.searchParams.append("longitude", this.longitude);
+  }
+
+  getUrl() {
+    return this.baseUrl;
+  }
+
+  getLatitude() {
+    return this.latitude;
+  }
+
+  getLongitude() {
+    return this.longitude;
+  }
+
+  setLatitude(latitude: string) {
+    this.latitude = latitude;
+    this.baseUrl.searchParams.set("latitude", this.latitude);
+  }
+
+  setLongitude(longitude: string) {
+    this.longitude = longitude;
+    this.baseUrl.searchParams.set("longitude", this.longitude);
+  }
+
+  async getCurrentForecast() {
+    this.baseUrl.searchParams.append("current_weather", "true");
+    this.baseUrl.searchParams.append(
+      "hourly",
+      "temperature_2m,relativehumidity_2m,windspeed_10m"
+    );
+    // this.baseUrl.searchParams.append("forecast_days", "7");
+    await fetch(this.baseUrl, {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
   }
 }
